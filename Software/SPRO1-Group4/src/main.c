ee3b96bf1eddef95d7443b2b9b2eeea425afa06f
bigIq <viktorio.k.milanov.2019@elsys-bg.org>
2024-11-23 20:59:35 +0100

    added gerber files

--- Software/SPRO1-Group4/src/main.c
+++ Software/SPRO1-Group4/src/main.c
@@ -8,10 +8,10 @@ void printBinary(uint8_t value);
 
 int main(void) {  
 
-  DDRB |= 1 << PB2;
-  DDRB |= 0 << PB5;
+  DDRB |= (1 << PB1) | (1 << PB5) | (1 << PB6);//brg
+  DDRC |= (0 << PC1) | (0 << PC2) | (0 << PC3);//rgb
   
-  float adc_value;
+  float red, green, blue;
 
   uart_init(); 
   io_redirect(); 
@@ -21,13 +21,17 @@ int main(void) {
   // LCD_init();
   
   while(1) {
-    adc_value = adc_read(0);
-    adc_value = adc_value*100/1023;
-    printf("ADC Value: %d\n", 100-(int)adc_value);
+    red = adc_read(1);
+    green = adc_read(2);
+    blue = adc_read(3);
+    red = red*100/1023;
+    green = green*100/1023;
+    blue = blue*100/1023;
+    printf("Red Value: %d\n", 100-(int)red);
     if(PINB & (1 << PB5))
     {
       printf("Detected.\n");
-      pwm3_set_duty(0,0,100-adc_value);
+      pwm3_set_duty(0,0,100-red);
       _delay_ms(100);
     }else{
       PORTB &= 0<<PB2;
