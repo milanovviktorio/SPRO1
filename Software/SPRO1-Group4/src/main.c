87ed56fdf85765413280eb82c06962958d806578
bigIq <viktorio.k.milanov.2019@elsys-bg.org>
2024-12-09 19:42:56 +0100

    definitions (questionable)

--- Software/SPRO1-Group4/src/main.c
+++ Software/SPRO1-Group4/src/main.c
@@ -21,14 +21,21 @@ int main(void) {
   //maindoor definitions
   DDRB |= 1 << PB2;//white main door
   DDRD |= 0 << PB0;//PIR
+  // DDRC |= 0 << PC6;//ldr
 
   //room1 definitions
+  DDRD |= 1 << PD3;//white room 1
+  DDRC |= 0 << PC0;//pot room 1
+  DDRB |= 1 << PB3 || 0 << PB5;//fan || switch room 1
 
   //room2 definitions
+  DDRD |= 1 << PD2 || 0 << PD7;//white room 2 || switch1 room 2
+  DDRB |= 0 << PB4;//switch2 room 2
 
   //room3 definitions
   DDRB |= (1 << PB1) | (1 << PB5) | (1 << PB6);//brg - diode
   DDRC |= (0 << PC1) | (0 << PC2) | (0 << PC3);//rgb - pots
+  DDRC |= 0 << 7;//switch room 3
 
   // uart_init(); 
   // io_redirect(); 
@@ -79,7 +86,7 @@ void mainDoor(void)
   _delay_ms(100);
 }
 
-void room3()
+void room3(void)
 {
   ldr_value = adc_read(6);//LDR
   ldr_value = ldr_value*100/1023;
